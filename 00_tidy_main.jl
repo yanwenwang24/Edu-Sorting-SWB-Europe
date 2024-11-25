1ce2e2e6c2f6912c152e7dfead43aaf4b4b6d4f9
Yanwen Wang <yanwenwang@u.nus.edu>
2024-11-25 11:58:08 -0500

    Add master script for tidying raw data

---
+++ 00_tidy_main.jl
@@ -0,0 +1,27 @@
+## ------------------------------------------------------------------------
+##
+## Script name: 00_tidy_main.jl
+## Purpose: Master file to tidy the raw data
+## Author: Yanwen Wang
+## Date Created: 2024-11-25
+## Email: yanwenwang@u.nus.edu
+##
+## ------------------------------------------------------------------------
+##
+## Notes:
+##
+## ------------------------------------------------------------------------
+
+# Load the required packages
+using Arrow
+using CategoricalArrays
+using DataFrames, DataFramesMeta
+using Random
+using RData
+using StatsBase
+
+# Load data, dictionaries and functions
+ESS = load("Datasets_raw/ESS.RData")
+
+# Source scripts
+@time include("01_tidy_ESS1.jl")
