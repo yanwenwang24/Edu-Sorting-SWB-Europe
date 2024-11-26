ecfa90b7251152ac98e03311b79d9cda44c8c7f0
Yanwen Wang <yanwenwang@u.nus.edu>
2024-11-25 19:59:33 -0500

    Add scripts for cleaning ESS3 data

--- 00_tidy_main.jl
+++ 00_tidy_main.jl
@@ -18,7 +18,6 @@ using CategoricalArrays
 using DataFrames, DataFramesMeta
 using Random
 using RData
-using StatsBase
 
 # Load data, dictionaries and functions
 ESS = load("Datasets_raw/ESS.RData")
@@ -27,3 +26,6 @@ ESS = load("Datasets_raw/ESS.RData")
 @time include("01_tidy_ESS1.jl")
 @time include("02_tidy_ESS2.jl")
 @time include("02.1_tidy_ESS2_IT.jl")
+@time include("03_tidy_ESS3.jl")
+@time include("03.1_tidy_ESS3_LV.jl")
+@time include("03.2_tidy_ESS3_RO.jl")
