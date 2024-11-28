410932235b953c7d49c7a63402e829e29c38fdbe
Yanwen Wang <yanwenwang@u.nus.edu>
2024-11-28 13:04:06 -0500

    Standardize age and household size for gender-specific samples in analysis script; remove redundant models

--- scripts_analysis/08_dmm_nodivorce.r
+++ scripts_analysis/08_dmm_nodivorce.r
@@ -24,10 +24,6 @@ library(tidyverse)
 # Load data
 sample <- read_feather("Datasets_tidy/sample.arrow")
 
-# Standardize age and household size
-sample$age_scale <- scale(sample$age)
-sample$hhsize_scale <- scale(sample$hhsize)
-
 # Categorize education
 sample <- sample %>%
   mutate(
@@ -45,52 +41,18 @@ sample <- select(sample, -essround_10, -cntry_AT)
 sample_men <- filter(sample, female == 0)
 sample_women <- filter(sample, female == 1)
 
+# Standardize age and household size
+sample_men$age_scale <- scale(sample_men$age)
+sample_women$age_scale <- scale(sample_women$age)
+sample_men$hhsize_scale <- scale(sample_men$hhsize)
+sample_women$hhsize_scale <- scale(sample_women$hhsize)
+
 # 2 Fomulas ---------------------------------------------------------------
-
-# Baseline
-fmla_base <- as.formula(paste0(
-  "lsat ~",
-  "-1 + age_scale + cohabit +
-  immigrant + minority + hhsize_scale + child_count + child_under6_present +
-  uempl + hincfel + ",
-  paste(grep("essround_", names(sample), value = TRUE), collapse = "+"),
-  " + ",
-  paste(grep("cntry_", names(sample), value = TRUE), collapse = "+"),
-  " + ",
-  "Dref(edu4_r, edu4_s)"
-))
-
-# Heterogamy
-fmla_heter <- as.formula(paste0(
-  "lsat ~",
-  "-1 + heter4 + homo4_index + hyper4_index + age_scale + cohabit +
-  immigrant + minority + hhsize_scale + child_count + child_under6_present +
-  uempl + hincfel + ",
-  paste(grep("essround_", names(sample), value = TRUE), collapse = "+"),
-  " + ",
-  paste(grep("cntry_", names(sample), value = TRUE), collapse = "+"),
-  " + ",
-  "Dref(edu4_r, edu4_s)"
-))
-
-fmla_heter_inter <- as.formula(paste0(
-  "lsat ~",
-  "-1 + heter4*homo4_index + heter*hyper4_index + age_scale + 
-  cohabit +
-  immigrant + minority + hhsize_scale + child_count + child_under6_present +
-  uempl + hincfel + ",
-  paste(grep("essround_", names(sample), value = TRUE), collapse = "+"),
-  " + ",
-  paste(grep("cntry_", names(sample), value = TRUE), collapse = "+"),
-  " + ",
-  "Dref(edu4_r, edu4_s)"
-))
 
 # Hypergamy and hypogamy
 fmla_hyper <- as.formula(paste0(
   "lsat ~",
-  "-1 + hyper4 + hypo4 + homo4_index + hyper4_index + age_scale + 
-  cohabit +
+  "-1 + hyper4 + hypo4 + age_scale + cohabit +
   immigrant + minority + hhsize_scale + child_count + child_under6_present +
   uempl + hincfel + ",
   paste(grep("essround_", names(sample), value = TRUE), collapse = "+"),
@@ -106,8 +68,6 @@ fmla_hyper_inter <- as.formula(paste0(
   hypo4*homo4_index + hypo4*hyper4_index + age_scale + cohabit +
   immigrant + minority + hhsize_scale + child_count + child_under6_present +
   uempl + hincfel + ",
-  paste(grep("essround_", names(sample), value = TRUE), collapse = "+"),
-  " + ",
   paste(grep("cntry_", names(sample), value = TRUE), collapse = "+"),
   " + ",
   "Dref(edu4_r, edu4_s)"
@@ -117,151 +77,7 @@ fmla_hyper_inter <- as.formula(paste0(
 
 set.seed(321)
 
-# 3.1 Baseline ------------------------------------------------------------
-
-# Men
-mod_base_men <- gnm(
-  fmla_base,
-  data = sample_men,
-  weights = anweight
-)
-
-summ(mod_base_men, digits = 3)
-print(DrefWeights(mod_base_men), digits = 3)
-
-# Women
-mod_base_women <- gnm(
-  fmla_base,
-  data = sample_women,
-  weights = anweight
-)
-
-summ(mod_base_women, digits = 3)
-print(DrefWeights(mod_base_women), digits = 3)
-
-# 3.2 Heterogamy ----------------------------------------------------------
-
-# 3.2.1 Main model --------------------------------------------------------
-
-# Men
-mod_heter_men <- gnm(
-  fmla_heter,
-  data = sample_men,
-  weights = anweight
-)
-
-summ(mod_heter_men, digits = 3)
-print(DrefWeights(mod_heter_men), digits = 3)
-
-# Women
-mod_heter_women <- gnm(
-  fmla_heter,
-  data = sample_women,
-  weights = anweight
-)
-
-summ(mod_heter_women, digits = 3)
-print(DrefWeights(mod_heter_women), digits = 3)
-
-# Compare men vs. women
-heter_men_df <- se(mod_heter_men) %>%
-  as.data.frame() %>%
-  rownames_to_column() %>%
-  rename(
-    estimate.men = Estimate,
-    se.men = `Std. Error`
-  )
-
-heter_women_df <- se(mod_heter_women) %>%
-  as.data.frame() %>%
-  rownames_to_column() %>%
-  rename(
-    estimate.women = Estimate,
-    se.women = `Std. Error`
-  )
-
-left_join(
-  heter_men_df,
-  heter_women_df,
-  by = "rowname"
-) %>%
-  mutate(z = (estimate.men - estimate.women) / sqrt(se.men^2 + se.women^2)) %>%
-  mutate(
-    p = 2 * pnorm(-abs(z)),
-    p = round(p, 3)
-  ) %>%
-  mutate(
-    star = case_when(
-      p < 0.001 ~ "***",
-      p < 0.01 ~ "**",
-      p < 0.05 ~ "*",
-      TRUE ~ "ns"
-    )
-  ) %>%
-  select(rowname, z, p, star)
-
-# 3.2.2 Interaction model ------------------------------------------------
-
-# Men
-mod_heter_inter_men <- gnm(
-  fmla_heter_inter,
-  data = sample_men,
-  weights = anweight
-)
-
-summ(mod_heter_inter_men, digits = 3)
-print(DrefWeights(mod_heter_inter_men), digits = 3)
-
-# Women
-mod_heter_inter_women <- gnm(
-  fmla_heter_inter,
-  data = sample_women,
-  weights = anweight
-)
-
-summ(mod_heter_inter_women, digits = 3)
-print(DrefWeights(mod_heter_inter_women), digits = 3)
-
-# Compare men vs. women
-heter_inter_men_df <- se(mod_heter_inter_men) %>%
-  as.data.frame() %>%
-  rownames_to_column() %>%
-  rename(
-    estimate.men = Estimate,
-    se.men = `Std. Error`
-  )
-
-heter_inter_women_df <- se(mod_heter_inter_women) %>%
-  as.data.frame() %>%
-  rownames_to_column() %>%
-  rename(
-    estimate.women = Estimate,
-    se.women = `Std. Error`
-  )
-
-left_join(
-  heter_inter_men_df,
-  heter_inter_women_df,
-  by = "rowname"
-) %>%
-  mutate(z = (estimate.men - estimate.women) / sqrt(se.men^2 + se.women^2)) %>%
-  mutate(
-    p = 2 * pnorm(-abs(z)),
-    p = round(p, 3)
-  ) %>%
-  mutate(
-    star = case_when(
-      p < 0.001 ~ "***",
-      p < 0.01 ~ "**",
-      p < 0.05 ~ "*",
-      TRUE ~ "ns"
-    )
-  ) %>%
-  select(rowname, z, p, star)
-
-# 3.3 Hypergamy and hypogamy --------------------------------------------
-
-# 3.3.1 Main model ------------------------------------------------------
+# 3.1 Main model ---------------------------------------------------------
 
 # Men
 mod_hyper_men <- gnm(
@@ -320,7 +136,7 @@ left_join(
   ) %>%
   select(rowname, z, p, star)
 
-# 3.3.2 Interaction model -----------------------------------------------
+# 3.2 Interaction model --------------------------------------------------
 
 # Men
 mod_hyper_inter_men <- gnm(
