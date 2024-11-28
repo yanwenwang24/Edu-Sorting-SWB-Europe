871de7defa8cb8e153a171c9a258b8e60dea94dd
Yanwen Wang <yanwenwang@u.nus.edu>
2024-11-27 21:00:57 -0500

    Visualize dmm results by region

---
+++ scripts_analysis/05.8_dmm_visualize.jl
@@ -0,0 +1,56 @@
+## ------------------------------------------------------------------------
+##
+## Script name: 05.8_dmm_visualize.jl
+## Purpose: Visualize model coefficients by region
+## Author: Yanwen Wang
+## Date Created: 2024-11-27
+## Email: yanwenwang@u.nus.edu
+##
+## ------------------------------------------------------------------------
+##
+## Notes:
+
+## ------------------------------------------------------------------------
+
+# Load data
+dmm_region_summary = DataFrame(Arrow.Table("Datasets_tidy/dmm_region_summary.arrow"))
+
+# Plot
+f = Figure(; size=(1200, 800), fontsize=12)
+
+by_region = data(dmm_region_summary) * (
+    mapping(
+        :region => "",
+        :coef => "Coefficient",
+        :se,
+        dodge_x=:gender => "Gender",
+        color=:gender => "Gender",
+        row=:pattern
+    ) *
+    visual(Errorbars) +
+    mapping(
+        :region => "",
+        :coef => "Coefficient",
+        dodge_x=:gender => "Gender",
+        color=:gender => "Gender",
+        row=:pattern
+    ) *
+    visual(Scatter)
+)
+
+hlines = mapping(0) * visual(HLines, color=(:grey, 0.5), linestyle=:dash)
+
+plt = draw!(
+    f[1, 1],
+    by_region + hlines_plt,
+    scales(
+        DodgeX=(; width=0.5),
+        Color=(; palette=["#fd7f6f", "#7eb0d5", "#b2e061"])
+    )
+)
+
+legend!(f[:, 2], by_cohort_plt)
+
+f
+
+save("graphs/dmm_region.png", f; px_per_unit=2)
